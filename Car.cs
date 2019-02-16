a87c26c6a0926e72974d4e3c78d8642c513d0d4e
Владислав Горохов <gbg.0101@mail.ru>
2019-02-16 14:23:56 +0600

    First commit

---
+++ Car.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Analytics.Interfaces;
+using Microsoft.Analytics.Types.Sql;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace XmlHomeWork
+{
+    public class Car
+    {
+        public string Mark { get; set; }
+        public int AmoutGears { get; set; }
+        public int MaxSpeed { get; set; }
+        public Engine Engine { get; set; }
+    }
+}
---
+++ Engine.cs
@@ -0,0 +1,8 @@
+﻿namespace XmlHomeWork
+{
+    public class Engine
+    {
+        public string Brand { get; set; }
+        public int AmountValves { get; set; }
+    }
+}
